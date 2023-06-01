37faf37bd525c535fec0c15f4372e4ed922eb4ed
Rabbipxd <130862563+RabbiPND@users.noreply.github.com>
2023-06-01 22:03:22 +0200

    Unfinished game
    unfinished game

--- Rock Paper Scissors.js
+++ Rock Paper Scissors.js
@@ -1,7 +1,26 @@
-function getComputerChoice() {
-    
-}
+let pick = ["rock", "paper", "scissor"]
 
-console.log("Rock")
-    console.log("Paper")
-    console.log("Scissors")
+function getComputerChoice (pick) {
+    return pick [Math.floor(Math.random()*pick.length)] 
+  }
+
+  //console.log(getComputerChoice(pick))
+
+  let playerSelection = "Rock"
+  let computerSelection = getComputerChoice (pick) 
+
+  function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissor") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissor" && computerSelection === "paper")
+    ) { 
+        return "You Win!";
+    }   else {
+        return "Computer wins!"
+    }
+}
+ 
+console.log(playRound (playerSelection, computerSelection))
