ec1e0c6680056108468eb508ac14d123d490dd61
Rabbipxd <130862563+RabbiPND@users.noreply.github.com>
2023-08-30 22:33:25 +0200

    updated game

--- Rock Paper Scissors.js
+++ Rock Paper Scissors.js
@@ -75,20 +75,93 @@
     ;
   }
 
+    const body = document.body
+    const playAgain = document.createElement('button')
+    playAgain.classList.add('re-start')
+    playAgain.textContent = "Re-Start"
+    playAgain.style.color =  "green"
+    playAgain.style.width = "150px"
+    playAgain.style.backgroundColor = "goldenrod"
+    playAgain.style.padding = "20px"
+    playAgain.style.fontWeight = "bold"
+
+    var centerX = (body.offsetWidth - playAgain.offsetWidth) / 2;
+    var centerY = (body.offsetHeight - playAgain.offsetHeight) / 2;
+
+    playAgain.style.position = "absolute";
+    playAgain.style.left = centerX + "px";
+    playAgain.style.top = centerY + "px";
+
 
 function endGame() {
 
-  let messageEl = document.getElementById("message-el")
   if (playerScore > computerScore) {
-    messageEl.textContent = "Congratulations! You are the winner of this game of Rock, Paper, Scissors. Well played!"
+    document.getElementById('container').style.display = 'none'
+    const winMessage = document.createElement('div')
+    winMessage.classList.add('win-message')
+    winMessage.textContent = "Congratulations! You are the winner of this game of Rock, Paper, Scissors. Well played!"
+    winMessage.style.color = "goldenrod"
+    winMessage.style.border = "2px solid white"
+    winMessage.style.padding = "20px"
+    winMessage.style.backgroundColor = "white"
+    winMessage.style.fontWeight = "bold"
+    winMessage.style.textAlign = "center"
+    body.append(winMessage)
+    
+    body.append(playAgain)
+
+    playAgain.addEventListener('click', () => {
+      document.querySelector('.re-start').addEventListener('click', function(){
+        window.location.reload();
+        return false;
+      })
+    })
   }
 
   else if(playerScore < computerScore) {
-    messageEl.textContent = "Oops! You lost this game of Rock, Paper, Scissors. Better luck next time!";  
+    document.getElementById('container').style.display = 'none'
+    const loseMessage = document.createElement('div')
+    loseMessage.classList.add('win-message')
+    loseMessage.textContent = "Oops! You lost this game of Rock, Paper, Scissors. Better luck next time!"
+    loseMessage.style.color = "goldenrod"
+    loseMessage.style.border = "2px solid white"
+    loseMessage.style.padding = "20px"
+    loseMessage.style.backgroundColor = "white"
+    loseMessage.style.fontWeight = "bold"
+    loseMessage.style.textAlign = "center"
+    body.append(loseMessage)
+    
+    body.append(playAgain)
+
+    playAgain.addEventListener('click', () => {
+      document.querySelector('.re-start').addEventListener('click', function(){
+        window.location.reload();
+        return false;
+      })
+    })
   }
 
   else {
-    messageEl.textContent = "It's a tie! The game ended in a draw.";
+    document.getElementById('container').style.display = 'none'
+    const tieMessage = document.createElement('div')
+    tieMessage.classList.add('win-message')
+    tieMessage.textContent = "It's a tie! The game ended in a draw."
+    tieMessage.style.color = "goldenrod"
+    tieMessage.style.border = "2px solid white"
+    tieMessage.style.padding = "20px"
+    tieMessage.style.backgroundColor = "white"
+    tieMessage.style.fontWeight = "bold"
+    tieMessage.style.textAlign = "center"
+    body.append(tieMessage)
+
+    body.append(playAgain)
+
+    playAgain.addEventListener('click', () => {
+      document.querySelector('.re-start').addEventListener('click', function(){
+        window.location.reload();
+        return false;
+      })
+    })
   }
 }
 
@@ -97,7 +170,7 @@ const rockButton = document.getElementById('Rock');
 const paperButton = document.getElementById('Paper');
 const scissorsButton = document.getElementById('Scissors')
 
-// Add event listeners to buttons
+
 rockButton.addEventListener('click', () => playGame('Rock'));
 paperButton.addEventListener('click', () => playGame('Paper'));
 scissorsButton.addEventListener('click', () => playGame('Scissors'));
